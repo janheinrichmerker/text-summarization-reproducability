54e1cff1f3ecf309bf66722404fa984f6efb74cb
Jan Heinrich Reimer <heinrich@reimer.family>
2021-03-14 17:20:44 +0100

    Add notebook

---
+++ src/notebook.jl
@@ -0,0 +1,30 @@
+### A Pluto.jl notebook ###
+# v0.12.21
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ aaf3f94e-84d9-11eb-325e-3d162a5a95a2
+using Transformers
+
+# ╔═╡ 702d3820-84d9-11eb-1895-1d00242e5363
+md"""
+# Reproducing Text Summarization with Pretrained Encoders
+
+This is a reproducability study on the ["Text Summarization with Pretrained Encoders"](https://doi.org/10.18653/v1/D19-1387) paper by Yang Liu and Mirella Lapata.
+"""
+
+# ╔═╡ 58544bfa-84d9-11eb-3426-b3b5cc2a19a8
+md"""
+## BERT Test
+Here we just test if BERT works.
+"""
+
+# ╔═╡ ccbdbf7e-84d9-11eb-39b5-b9dd61cc9544
+
+
+# ╔═╡ Cell order:
+# ╟─702d3820-84d9-11eb-1895-1d00242e5363
+# ╟─58544bfa-84d9-11eb-3426-b3b5cc2a19a8
+# ╠═aaf3f94e-84d9-11eb-325e-3d162a5a95a2
+# ╠═ccbdbf7e-84d9-11eb-39b5-b9dd61cc9544
