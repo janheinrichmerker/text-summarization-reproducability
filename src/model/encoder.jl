1cbc91183bb4c0eb1351c10ec40fc6482f8eacfd
Jan Heinrich Reimer <heinrich@reimer.family>
2021-03-17 01:48:15 +0100

    Add encoder model

---
+++ src/model/encoder.jl
@@ -0,0 +1,28 @@
+using Transformers.Basic
+using Transformers.Basic:AbstractEmbed
+using Flux:@functor
+
+struct Encoder
+    embed::AbstractEmbed
+    layers::Array{<:Transformer} 
+end
+
+@functor Encoder
+
+function (encoder::Encoder)(indices)
+    embedding = encoder.embed(indices)
+    for transformer ∈ encoder.layers
+        embedding = transformer(embedding)
+    end
+    return embedding
+end
+
+function Encoder(embed::AbstractEmbed, size::Int, head::Int, hs::Int, ps::Int, layer::Int; act=relu, pdrop=0.1) 
+    return Encoder(
+        embed,
+        [
+            Transformer(size, head, hs, ps, act=act, pdrop=pdrop)
+            for i ∈ 1:layer
+        ]
+    )
+end
