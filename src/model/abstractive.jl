ae555674e89b388948f573ffd2e8840ac191715c
Jan Heinrich Reimer <heinrich@reimer.family>
2021-03-17 02:00:44 +0100

    Add abstractive models

--- src/model/abstractive.jl
+++ src/model/abstractive.jl
@@ -1,4 +1,26 @@
 using Transformers
+using Transformers.Basic
+using Flux
+using Flux:@functor, onecold
 
-function AbstractiveSummarizer(bert::Bert)
+include("decoder.jl")
+include("classifier.jl")
+include("translator.jl")
+
+# TransformerAbs model from the "Text Summarization with Pretrained Encoders" paper by Liu et al. (2019) as described on pages 6-7.
+function TransformerAbs(
+    embed::AbstractEmbed,
+    vocabulary::Vocabulary,
+)
+    return Translator(embed, vocabulary, 768, 8, 96, 2048, 6, pdrop=0.1)
+end
+
+# BertAbs model from the "Text Summarization with Pretrained Encoders" paper by Liu et al. (2019) as described on page 6.
+function BertAbs(bert_model::TransformerModel{AbstractEmbed,Bert,Any}, vocabulary::Vocabulary)
+    return Translator(
+        bert_model.Transformers,
+        Decoder(bert_model.embed, 768, 8, 96, 2048, 6, pdrop=0.1),
+        Classifier(length(vocabulary), size),
+        vocabulary
+    )
 end
