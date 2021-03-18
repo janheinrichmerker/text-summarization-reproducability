945f6deac693097c457435a56cfb64d0bf5ae819
Jan Heinrich Reimer <heinrich@reimer.family>
2021-03-18 03:14:25 +0100

    Refactor model test script

--- src/training_test.jl
+++ src/training_test.jl
@@ -1,6 +1,6 @@
 @info "Loading Flux."
 using Flux
-using Flux:onecold
+using Flux:onecold,gpu
 @info "Loading CUDA."
 using CUDA
 @info "Loading Transformers."
@@ -8,11 +8,8 @@ using Transformers
 using Transformers.Basic
 using Transformers.Pretrain
 
-
 # Enable GPU for Transformers.
 enable_gpu(true)
-gpu = todevice
-
 
 @info "Loading pretrained BERT model."
 bert_model, wordpiece, tokenizer = pretrain"bert-uncased_L-12_H-768_A-12"
@@ -25,11 +22,14 @@ sample = sample_text |> tokenizer |> wordpiece |> t -> ["[CLS]", t..., "[SEP]"]
 # include("src/model/abstractive.jl")
 include("model/abstractive.jl")
 
-model::Translator = BertAbs(
+model = BertAbs(
     bert_model,
     length(vocabulary),
-    5 # TODO this parameter doesn't appear in the paper.
-) # |> gpu
+    # This parameter is not described in the paper.
+    # Though it seems reasonable that for training 
+    # it should roughly match the target sequence length.
+    length(sample),
+    length_normalization=0.8,
+)
 
-sample
 translated = model(sample, vocabulary)
