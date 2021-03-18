45e280b8a421e5b36cc92da03d8a5e7ce9cece28
Jan Heinrich Reimer <heinrich@reimer.family>
2021-03-18 01:19:27 +0100

    Use beam search model in translator

--- src/model/translator.jl
+++ src/model/translator.jl
@@ -6,40 +6,57 @@ using Flux: @functor, onecold
 include("encoder.jl")
 include("decoder.jl")
 include("classifier.jl")
-include("../search/beam_search.jl")
+include("beam_search.jl")
 
 struct Translator
+    embed::AbstractEmbed
     encoder::Union{Encoder,Bert}
     decoder::Decoder
     classifier::Classifier
-    vocabulary::Vocabulary
+    beam_search::BeamSearch
 end
 
 @functor Translator
 
-function (translator::Translator)(tokens, start_token, end_token)
-    indices = tokens |> translator.vocabulary
-    encoded = translator.encoder(indices)    
-    sequence = beam_search(
-        5,
+function (translator::Translator)(
+    tokens::AbstractVector{T};
+    vocabulary::Vocabulary{T},
+    start_token::T="[CLS]",
+    end_token::T="[SEP]"
+)::AbstractVector{T} where T
+    function prepare(tokens::AbstractVector{T})
+        indices = translator.vocabulary(tokens)
+        if typeof(translator.embed) <: CompositeEmbedding
+            indices = (tok = indices, segment = fill(1, length(indices)))
+        end
+        return translator.embed(indices)
+    end
+
+    embeddings = prepare(tokens)
+    encoded = translator.encoder(embeddings)
+
+    function predict(sequence::AbstractVector{T})
+        target = prepare(sequence)
+        decoded = translator.decoder(target, encoded)
+        return translator.classifier(decoded)[:,end]
+    end
+
+    sequence = translator.beam_search(
         translator.vocabulary.list,
-        function (sequence::Array)
-            target = translator.vocabulary(sequence)
-            decoded = translator.decoder(target, encoded)
-            classified = translator.classifier(decoded)[:,end]
-            return classified
-        end,
-        sequence -> last(sequence) != end_token,
-        initial_sequence=[start_token]
+        predict,
+        start_token,
+        end_token,
     )
-    return sequence[2:end - 1]
+    return sequence
 end
 
-function Translator(embed::AbstractEmbed, vocabulary::Vocabulary, size::Int, head::Int, hs::Int, ps::Int, layer::Int; act=relu,pdrop=0.1)
+function Translator(embed::AbstractEmbed, vocab_size::Int, beam_width::Int, max_length::Int, size::Int, head::Int, hs::Int, ps::Int, layer::Int; act=relu,pdrop=0.1)
     return Translator(
-        Encoder(embed, size, head, hs, ps, layer; act=act,pdrop=pdrop),
-        Decoder(embed, size, head, hs, ps, layer; act=act,pdrop=pdrop),
+        embed,
+        Encoder(size, head, hs, ps, layer; act=act,pdrop=pdrop),
+        Decoder(size, head, hs, ps, layer; act=act,pdrop=pdrop),
         Classifier(length(vocabulary), size),
-        vocabulary
+        BeamSearch(beam_width, max_length)
     )
 end
+
