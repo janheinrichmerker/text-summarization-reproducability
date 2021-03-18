fb283bad726b8b2a368a86b3a7874a5181922375
Jan Heinrich Reimer <heinrich@reimer.family>
2021-03-18 01:15:03 +0100

    Remove embedding from encoder

--- src/model/encoder.jl
+++ src/model/encoder.jl
@@ -1,35 +1,30 @@
 using Transformers.Basic
-using Transformers.Basic:AbstractEmbed
 using Flux:@functor
 
 struct Encoder
-    embed::AbstractEmbed
     layers::Array{<:Transformer} 
 end
 
 @functor Encoder
 
-function (encoder::Encoder)(indices)
-    if typeof(encoder.embed) <: CompositeEmbedding
-        indices = (
-            tok = indices,
-            segment = fill(1, length(indices))
-        )
-    end
-    
-    embedding = encoder.embed(indices)
+function (encoder::Encoder)(embedding)
     for transformer ∈ encoder.layers
         embedding = transformer(embedding)
     end
     return embedding
 end
 
-function Encoder(embed::AbstractEmbed, size::Int, head::Int, hs::Int, ps::Int, layer::Int; act=relu, pdrop=0.1) 
-    return Encoder(
-        embed,
-        [
-            Transformer(size, head, hs, ps, act=act, pdrop=pdrop)
-            for i ∈ 1:layer
-        ]
-    )
+function Encoder(
+    size::Int, 
+    head::Int, 
+    hs::Int, 
+    ps::Int, 
+    layer::Int; 
+    act=relu, 
+    pdrop=0.1
+)::Encoder
+    return Encoder([
+        Transformer(size, head, hs, ps, act=act, pdrop=pdrop)
+        for i ∈ 1:layer
+    ])
 end
