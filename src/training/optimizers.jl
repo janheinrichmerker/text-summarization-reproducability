bf87f90bd0e8d528186364a6b4b4104259f4a1b1
Jan Heinrich Reimer <heinrich@reimer.family>
2021-03-20 06:04:55 +0100

    Add warmup schedule and optimizer

---
+++ src/training/optimizers.jl
@@ -0,0 +1,29 @@
+using Flux
+using ParameterSchedulers
+
+struct Warmup
+    learning_rate::Float64
+    warmup::Float64
+end
+
+function (schedule::Warmup)(t)
+    schedule.learning_rate * min(t^(-0.5), t * schedule.warmup^(-1.5))
+end
+
+Base.iterate(schedule::Warmup, t=1) = schedule(t), t + 1
+
+Warmup(;η=0.001, w=10_000) = Warmup(η, w)
+
+struct WarmupADAM
+    adam::ADAM
+    warmup::Warmup
+end
+
+WarmupADAM(η=0.001, w=10_000, β=(0.9, 0.999)) = WarmupADAM(ADAM(0.0, β), Warmup(η, w))
+
+WarmupADAM(;η=0.001, w=10_000, β=(0.9, 0.999)) = WarmupADAM(η, w, β)
+
+function Flux.Optimise.apply!(opt::WarmupADAM, x, Δ)
+    opt.adam.eta = next!(opt.warmup)
+    return Flux.Optimise.apply!(opt.adam, x, Δ)
+end
