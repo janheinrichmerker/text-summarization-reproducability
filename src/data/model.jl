2fee9565e77053eb37bdfbe189b65551bf1e55f5
Jan Heinrich Reimer <heinrich@reimer.family>
2021-03-20 06:02:09 +0100

    Fix model

--- src/data/model.jl
+++ src/data/model.jl
@@ -5,7 +5,7 @@ end
 
 # Opinionated replacement/joining of preprocessed 
 # source and target sequences.
-function SummaryPair(dict::Dict{String,Any})
+function SummaryPair(dict::Dict{Any,Any})
     source = join(dict["src_txt"], " ")
     target = replace(dict["tgt_txt"], "<q>" => " . ")
     SummaryPair(source, target)
