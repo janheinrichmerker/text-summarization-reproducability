7705f1d429d6646d4128c8bf6c4b058cd258a0de
Jan Heinrich Reimer <heinrich@reimer.family>
2021-03-20 22:23:25 +0100

    Fix install script

--- install-julia.sh
+++ install-julia.sh
@@ -7,9 +7,9 @@ VERSION="1.5.3"
 if [ -z `which julia` ]; then
   echo "Installing Julia ..."
   URL="https://julialang-s3.julialang.org/bin/linux/x64/$(cut -d '.' -f -2 <<< "$VERSION")/julia-$VERSION-linux-x86_64.tar.gz"
-  mkdir ./tmp
+  mkdir -p ./tmp
   wget -nv $URL -O ./tmp/julia.tar.gz
-  mkdir "./julia-$VERSION"
+  mkdir -p "./julia-$VERSION"
   tar -x -f ./tmp/julia.tar.gz -C "./julia-$VERSION" --strip-components 1
   rm ./tmp/julia.tar.gz
   ln -s "./julia-$VERSION/bin/julia" ./julia
