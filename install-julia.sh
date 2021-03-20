70fab962a3032d65b0155ea5b60bb1aab6d4a405
Jan Heinrich Reimer <heinrich@reimer.family>
2021-03-20 22:03:19 +0100

    Add script for installing Julia locally

---
+++ install-julia.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+VERSION="1.5.3"
+
+if [ -z `which julia` ]; then
+  echo "Installing Julia ..."
+  URL="https://julialang-s3.julialang.org/bin/linux/x64/$(cut -d '.' -f -2 <<< "$VERSION")/julia-$VERSION-linux-x86_64.tar.gz"
+  mkdir ./tmp
+  wget -nv $URL -O ./tmp/julia.tar.gz
+  tar -x -f ./tmp/julia.tar.gz -C "./julia-$VERSION" --strip-components 1
+  rm ./tmp/julia.tar.gz
+  ln -s "./julia-$VERSION/bin/julia" ./julia
+  echo "Finished installing Julia."
+fi
