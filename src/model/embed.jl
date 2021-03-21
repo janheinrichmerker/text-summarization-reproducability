5083c72a703225009780c7628d1b9898ebf399b6
Jan Heinrich Reimer <heinrich@reimer.family>
2021-03-21 04:54:37 +0100

    Fix embeds

--- src/model/embed.jl
+++ src/model/embed.jl
@@ -9,8 +9,8 @@ end
 Flux.@functor WordPositionEmbed
 Flux.trainable(embed::WordPositionEmbed) = (embed.word, embed.position)
 
-function (embed::WordPositionEmbed)(x)
-    word_embedding = embed.word(x) 
+function (embed::WordPositionEmbed)(indices::AbstractArray{<:Number})
+    word_embedding = embed.word(indices) 
     embedding = word_embedding .+ embed.position(word_embedding)
     return embedding
 end
@@ -23,7 +23,7 @@ function WordPositionEmbed(
     scale=inv(sqrt(size))
 )::WordPositionEmbed
     return WordPositionEmbed(
-        Embed(size, vocab_size),
+        Embed(size, vocab_size, scale=scale),
         PositionEmbedding(size, max_len, trainable=trainable)
     )
 end
