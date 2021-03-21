2c0da2f8f412285a582b668713ba5f184545a1d2
Jan Heinrich Reimer <heinrich@reimer.family>
2021-03-21 02:39:39 +0100

    Add word and position embedding model

---
+++ src/model/embed.jl
@@ -0,0 +1,29 @@
+using Transformers.Basic
+using Transformers.Basic:AbstractEmbed
+using Flux:@functor
+
+struct WordPositionEmbed <: AbstractEmbed
+    word::Embed
+    position::PositionEmbedding
+end
+
+@functor WordPositionEmbed
+
+function (embed::WordPositionEmbed)(x)
+    word_embedding = embed.word(x) 
+    embedding = word_embedding .+ embed.position(word_embedding)
+    return embedding
+end
+
+function WordPositionEmbed(
+    size::Int, 
+    vocab_size::Int, 
+    max_len::Int=1024;
+    trainable::Bool=false,
+    scale=inv(sqrt(size))
+)::WordPositionEmbed
+    return WordPositionEmbed(
+        Embed(size, vocab_size),
+        PositionEmbedding(size, max_len, trainable=trainable)
+    )
+end
