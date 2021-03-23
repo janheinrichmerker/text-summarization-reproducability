8a26fdbc3c7d56a0a4141ddfc7f588c3166f533e
Jan Heinrich Reimer <heinrich@reimer.family>
2021-03-23 17:28:03 +0100

    Add common train/eval functions

---
+++ src/training/common.jl
@@ -0,0 +1,26 @@
+using Base:AbstractVector,AbstractMatrix
+using Transformers.Basic
+using Transformers.BidirectionalEncoder:WordPiece
+
+include("../model/translator.jl")
+include("loss.jl")
+
+function preprocess(text::String, wordpiece::WordPiece, tokenizer::Function)::AbstractVector{String}
+    tokens = text |> tokenizer |> wordpiece
+    max_length = min(4096, length(tokens)) # Truncate to 4096 tokens
+    tokens = tokens[1:max_length]
+    return ["[CLS]"; tokens; "[SEP]"]
+end
+
+label_smoothing_α = 0.0 # Label smoothing doesn't work yet.
+function loss(
+    inputs::AbstractVector{String},
+    outputs::AbstractVector{String},
+    ground_truth::AbstractMatrix{<:Number},
+    model::Translator,
+    vocabulary::Vocabulary,
+)::AbstractFloat
+    prediction = model.transformers(vocabulary, inputs, outputs)
+    loss = logtranslationloss(prediction, ground_truth, α=label_smoothing_α)
+    return loss
+end
