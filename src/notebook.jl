1a6d36cd1e5205aa66b3126ac0c856b380a9a134
Jan Heinrich Reimer <heinrich@reimer.family>
2021-03-23 20:06:39 +0100

    Clean up data setup in notebook

--- src/notebook.jl
+++ src/notebook.jl
@@ -13,11 +13,23 @@ using GPUArrays: allowscalar
 # ╔═╡ 4068759c-8c08-11eb-1d2f-d79efe98c2b0
 using Flux
 
+# ╔═╡ 8297be82-8c08-11eb-39fb-eb37f75d09e4
+using Flux: update!, reset!, onehot
+
+# ╔═╡ 91e66b2c-8c08-11eb-1a3e-891e6401eb8b
+using Transformers
+
+# ╔═╡ 92baa6f8-8c08-11eb-3321-233e7bf14afe
+using Transformers.Basic
+
+# ╔═╡ 95f72c2e-8c08-11eb-2e18-0b166f648de6
+using Transformers.Pretrain
+
+# ╔═╡ 98c4351e-8c08-11eb-14fe-7df792e4b7cf
+using BSON: @save, @load
+
 # ╔═╡ 38f9317a-84e2-11eb-117b-9f62916abd75
 using DataDeps
-
-# ╔═╡ ccbdbf7e-84d9-11eb-39b5-b9dd61cc9544
-include("data/datasets.jl");
 
 # ╔═╡ 702d3820-84d9-11eb-1895-1d00242e5363
 md"""
@@ -30,6 +42,9 @@ This is a reproducability study on the ["Text Summarization with Pretrained Enco
 md"""
 ## Setup
 """
+
+# ╔═╡ f2ef12a6-8c09-11eb-10ae-0177f2691fdf
+DEBUG=true
 
 # ╔═╡ 3351b1ca-8c08-11eb-14c3-8f61900721f4
 md"""
@@ -53,9 +68,6 @@ else
     enable_gpu(false)
 end
 
-# ╔═╡ 2ca30702-8c08-11eb-31aa-b55c7ce561fb
-
-
 # ╔═╡ 22e29d1a-84e2-11eb-3e35-f7050cfd6cc4
 md"""
 ## Datasets
@@ -70,40 +82,23 @@ ENV["DATADEPS_ALWAYS_ACCEPT"] = true; # Don't ask for downloading.
 # ╔═╡ 29348670-84e4-11eb-076b-b78a92e94642
 ENV["DATADEPS_PROGRESS_UPDATE_PERIOD"] = 5; # Log process to the console, though.
 
-# ╔═╡ 2e4da306-85d6-11eb-0957-1182af2f9302
-md"Include data dependency definitions."
-
 # ╔═╡ 98474e70-8c06-11eb-28c4-35bd54c8a558
 md"""
 ### Data loading
 """
 
-# ╔═╡ 430bfb54-84e3-11eb-382d-0b842b8c1e06
-md"Load preprocessed XSum data."
+# ╔═╡ 2e4da306-85d6-11eb-0957-1182af2f9302
+md"""
+Include data dependency definitions.
+To keep the notebook readable, data loading is included from a separate file.
+(Note the usage of `ingredients()` instead of `include()`: This is needed to avoid namespace clashes and allow for re-executing the cell.)
+"""
 
-# ╔═╡ 554263ca-84e1-11eb-1dfa-a346284f5891
-data_xsum = joinpath(
-	datadep"XSum-Preprocessed-BERT",
-	"bert_data_xsum_new"
-)
-
-# ╔═╡ e0c16654-8526-11eb-054e-0796d0c4cc43
-readdir(data_xsum)
+# ╔═╡ 32af5d7e-8c0a-11eb-1784-17984db6e6fc
+cnndm_train = nothing
 
 # ╔═╡ 6462ab2e-84e3-11eb-33dd-13e5000c78da
 md"Load preprocessed CNN/Dailymail data."
-
-# ╔═╡ 41c97f76-84e2-11eb-38d3-d5a96f75c59a
-data_cnndm = joinpath(
-	datadep"CNN-Dailymail-Preprocessed-BERT",
-	"bert_data_cnndm_final"
-)
-
-# ╔═╡ 25e67bca-8527-11eb-2736-5362c6ce3725
-readdir(data_cnndm)
-
-# ╔═╡ ee8b8128-85d5-11eb-03b4-4153ea6de940
-md"Include data loading utils for loading preprocessed data."
 
 # ╔═╡ 58544bfa-84d9-11eb-3426-b3b5cc2a19a8
 md"""
@@ -185,34 +180,33 @@ function ingredients(path::String) # See
 	m
 end;
 
-# ╔═╡ f8ea1b8a-8536-11eb-3f98-3b8e28ac16ff
-loader = ingredients("data/loader.jl");
+# ╔═╡ 2ca30702-8c08-11eb-31aa-b55c7ce561fb
+data = ingredients("data/datasets.jl");
 
 # ╔═╡ Cell order:
 # ╟─702d3820-84d9-11eb-1895-1d00242e5363
 # ╟─176ccb48-8c08-11eb-3068-3b684ff378b5
+# ╠═f2ef12a6-8c09-11eb-10ae-0177f2691fdf
 # ╟─3351b1ca-8c08-11eb-14c3-8f61900721f4
 # ╠═2f26e06e-8c08-11eb-18c0-2f8e5b0cf47a
 # ╠═590fdac2-8c08-11eb-1c42-93ff500817c1
 # ╠═4068759c-8c08-11eb-1d2f-d79efe98c2b0
+# ╠═8297be82-8c08-11eb-39fb-eb37f75d09e4
+# ╠═91e66b2c-8c08-11eb-1a3e-891e6401eb8b
+# ╠═92baa6f8-8c08-11eb-3321-233e7bf14afe
+# ╠═95f72c2e-8c08-11eb-2e18-0b166f648de6
+# ╠═98c4351e-8c08-11eb-14fe-7df792e4b7cf
 # ╟─1be31330-8c08-11eb-296f-6f5df8bf6e41
 # ╠═2646eafe-8c08-11eb-2a25-c338673a3d2a
-# ╠═2ca30702-8c08-11eb-31aa-b55c7ce561fb
 # ╟─22e29d1a-84e2-11eb-3e35-f7050cfd6cc4
 # ╠═38f9317a-84e2-11eb-117b-9f62916abd75
 # ╠═0d86a904-84e3-11eb-1c0c-9d37637c8420
 # ╠═29348670-84e4-11eb-076b-b78a92e94642
-# ╟─2e4da306-85d6-11eb-0957-1182af2f9302
-# ╠═ccbdbf7e-84d9-11eb-39b5-b9dd61cc9544
 # ╟─98474e70-8c06-11eb-28c4-35bd54c8a558
-# ╟─430bfb54-84e3-11eb-382d-0b842b8c1e06
-# ╠═554263ca-84e1-11eb-1dfa-a346284f5891
-# ╟─e0c16654-8526-11eb-054e-0796d0c4cc43
+# ╟─2e4da306-85d6-11eb-0957-1182af2f9302
+# ╠═2ca30702-8c08-11eb-31aa-b55c7ce561fb
+# ╠═32af5d7e-8c0a-11eb-1784-17984db6e6fc
 # ╟─6462ab2e-84e3-11eb-33dd-13e5000c78da
-# ╠═41c97f76-84e2-11eb-38d3-d5a96f75c59a
-# ╟─25e67bca-8527-11eb-2736-5362c6ce3725
-# ╟─ee8b8128-85d5-11eb-03b4-4153ea6de940
-# ╠═f8ea1b8a-8536-11eb-3f98-3b8e28ac16ff
 # ╟─58544bfa-84d9-11eb-3426-b3b5cc2a19a8
 # ╟─a4f8ec0a-8c06-11eb-042e-290b3405e5e6
 # ╟─3f4853dc-8c06-11eb-3a59-cdb9fc854879
